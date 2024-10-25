260d0946b9218ae98978570c4843d3a85a3fbec7
Youri de Beurs <youri.debeurs@gmail.com>
2024-10-25 13:12:45 +0200

    Implemented RetryHandler in BlobStorageSteps

--- IntegrateMe.Azure.BlobStorage/BlobStorageStep.cs
+++ IntegrateMe.Azure.BlobStorage/BlobStorageStep.cs
@@ -1,5 +1,7 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure;
+using Azure.Storage.Blobs;
 using IntegrateMe.Core;
+using IntegrateMe.Core.Retry;
 
 namespace IntegrateMe.Azure.BlobStorage;
 
@@ -29,49 +31,131 @@ public class BlobStorageStep(AbstractStep parent) : AbstractStep(parent)
 
     public BlobStorageStep UploadBlob(string blobName, string data)
     {
+        return UploadBlob(blobName, data, RetryHandler.DefaultRetryHandler());
+    }
+
+    public BlobStorageStep UploadBlob(string blobName, string data, RetryHandler retryHandler)
+    {
         MainDsl.AddAction(async () =>
         {
-            var container = new BlobContainerClient(_connectionString, _containerName);
-            var blobClient = container.GetBlobClient(blobName);
-            await blobClient.UploadAsync(data);
+            await retryHandler.RunAsync(async () =>
+            {
+                try
+                {
+                    var container = new BlobContainerClient(_connectionString, _containerName);
+                    var blobClient = container.GetBlobClient(blobName);
+                    await blobClient.UploadAsync(data);
+                    retryHandler.Success();
+                }
+                catch (RequestFailedException)
+                {
+                }
+            });
+
+            if (!retryHandler.IsSuccess())
+            {
+                throw new Exception($"Failed to upload blob with name {blobName}");
+            }
         });
         return this;
     }
 
     public BlobStorageStep UploadBlob(string blobName, Stream data)
     {
+        return UploadBlob(blobName, data, RetryHandler.DefaultRetryHandler());
+    }
+
+    public BlobStorageStep UploadBlob(string blobName, Stream data, RetryHandler retryHandler)
+    {
         MainDsl.AddAction(async () =>
         {
-            var container = new BlobContainerClient(_connectionString, _containerName);
-            var blobClient = container.GetBlobClient(blobName);
-            await blobClient.UploadAsync(data);
+            await retryHandler.RunAsync(async () =>
+            {
+                try
+                {
+                    var container = new BlobContainerClient(_connectionString, _containerName);
+                    var blobClient = container.GetBlobClient(blobName);
+                    await blobClient.UploadAsync(data);
+                    retryHandler.Success();
+                }
+                catch (RequestFailedException)
+                {
+                }
+            });
+
+            if (!retryHandler.IsSuccess())
+            {
+                throw new Exception($"Failed to upload blob with name {blobName}");
+            }
         });
         return this;
     }
 
     public BlobStorageStep BlobExists(string blobName, bool exists = true)
     {
+        return BlobExists(blobName, exists, RetryHandler.DefaultRetryHandler());
+    }
+
+    public BlobStorageStep BlobExists(string blobName, bool exists, RetryHandler retryHandler)
+    {
         MainDsl.AddAction(async () =>
         {
+            var responseValue = !exists;
+
             var container = new BlobContainerClient(_connectionString, _containerName);
             var blobClient = container.GetBlobClient(blobName);
-            var response = await blobClient.ExistsAsync();
-            if (response.Value != exists)
+
+            await retryHandler.RunAsync(async () =>
             {
-                throw new Exception($"Blob {blobName} exists: {response.Value} but expected {exists}");
+                var response = await blobClient.ExistsAsync();
+                if (response.Value == exists)
+                {
+                    retryHandler.Success();
+                }
+
+                responseValue = response.Value;
+            });
+
+            if (!retryHandler.IsSuccess())
+            {
+                throw new Exception($"Blob {blobName} exists: {responseValue} but expected {exists}");
             }
         });
+
         return this;
     }
 
     public BlobStorageStep DeleteBlob(string blobName)
     {
+        return DeleteBlob(blobName, RetryHandler.DefaultRetryHandler());
+    }
+
+    public BlobStorageStep DeleteBlob(string blobName, RetryHandler retryHandler)
+    {
         MainDsl.AddAction(async () =>
         {
             var container = new BlobContainerClient(_connectionString, _containerName);
             var blobClient = container.GetBlobClient(blobName);
-            await blobClient.DeleteIfExistsAsync();
+            await retryHandler.RunAsync(async () =>
+            {
+                try
+                {
+                    await blobClient.DeleteIfExistsAsync();
+                    retryHandler.Success();
+                }
+                catch (RequestFailedException)
+                {
+                }
+
+                retryHandler.Success();
+            });
         });
+
+        if (!retryHandler.IsSuccess())
+        {
+            throw new Exception($"Failed to delete blob with name {blobName}");
+        }
+
         return this;
     }
 
